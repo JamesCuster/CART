eb09196be975d754eecfc64420518e062bf0d82b
James Custer <jamescuster0121@gmail.com>
2019-07-24 09:13:44 -0500

    create serverScript directory and serverAddProject.r

--- app/server.R
+++ app/server.R
@@ -43,30 +43,13 @@ shinyServer(
     # # monitor database for changes
     # loaddb <- reactivePoll(1000, session)
 
-# Reactives for addProject ------------------------------------------------
-    
-    
-    # Creates reactive when addProject/Time submit buttons are pressed
-    cleanProjectFormData <-
-      reactive({
-        projectFormResponse <- sapply(addProjectFields, function(x) {
-          if (grepl("date", x, ignore.case = TRUE)) {
-            as.character(input[[x]])
-          } 
-          else if (grepl("projectID", x)) {
-            NA
-          } 
-          else if (grepl("bdshLead", x)) {
-            people[people$name == input[[x]], "uteid", drop = TRUE]
-          } 
-          else {
-            input[[x]]
-          }
-        })
-        projectFormResponse
-      })
 
+# Server Scripts ----------------------------------------------------------
 
+    source(
+      "C:/Users/jmc6538/Desktop/BDSHProjectTracking/app/serverScripts/serverAddProject.R", 
+      local = TRUE)
+    
     # This is an attempt to add "add researcher" option to add project tab
     # observeEvent({
     #   if ((input$submitNewResearcher) || (input$projectPI == "Add Researcher")) TRUE
@@ -87,43 +70,7 @@ shinyServer(
     # )
     
     
-# what happens when submit button on Add Projects is pressed
-    observeEvent(
-      input$submitAddProject, {
-        # creates and displays table of inputs
-        saveProjectFormData(cleanProjectFormData())
-        
-        # Clears data from the forms
-        sapply(addProjectFields, function(x) {
-          updateTextInput(session, x, value = "")
-          session$sendCustomMessage(type = "resetValue", message = x)
-        })
-        
-        # creates button to submit data to database once a form is submitted
-        output$projectToDatabase <- renderUI({
-          actionButton("projectToDatabase", "Add data above to the database")
-        })
-      }
-    )
-    
-    output$projectFormResponses <- DT::renderDataTable({
-      input$submitAddProject
-      loadProjectFormData()})
-    
-    observeEvent(
-      input$projectToDatabase, {
-        dbWriteTable(BDSHProjects, "projects", projectFormData, append = TRUE)
-        projectFormData <<- projectFormData[c(), ]
-        
-        output$projectFormResponses <- DT::renderDataTable({
-          input$submitAddProject
-          loadProjectFormData()})
-        
-        # reload database
-        loadDatabase()
-        updateSelectInput(session, "projectID", "Select the project to add time to", projects$projectName)
-      }
-    )
+
 
     
 # Reactives for add time --------------------------------------------------
