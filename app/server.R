3140190ae6ac3c589a9fadb7cdbb2f1d6e1f0cb8
James Custer <jamescuster0121@gmail.com>
2019-08-13 11:29:08 -0500

    change how selectInput choices are updated, create observeEvents for each projects, researchers, and employees, that are triggered when respective reactiveData object changes

--- app/server.R
+++ app/server.R
@@ -37,8 +37,6 @@ shinyServer(
         valueFunc = function() {
           loadDatabase(tables = modified$tableName)
           updateOnLoad$dropdown <- TRUE
-          updateOnLoad$viewProjects <- TRUE
-          updateOnLoad$viewTime <- TRUE
         }
       )
     
@@ -116,12 +114,11 @@ shinyServer(
            })
     
     
-# 2.3 Update Dropdown Menu Reactive ---------------------------------------
-    # Reactives that trigger after new data is loaded from the database that update
-    # the necessary input dropdown menus to reflect any changes in the database
-    updateSelectDropdownMenus <- 
-      reactive({
-      # When new employee data is fetched from database
+    
+
+# update employee dependent inputs ----------------------------------------
+    observeEvent(
+      reactiveData$employees, {
         # Update selection inputs in the Add Project form
         updateSelectizeInput(
           session,
@@ -185,120 +182,304 @@ shinyServer(
           selected = dropdownMenuSelections[["viewTimeByEmployee"]],
           server = TRUE
         )
+      }
+    )
+    
+
+# update researcher dependent inputs --------------------------------------
+observeEvent(
+  reactiveData$researchers, {
+    # update selection inputs in the add project form
+    updateSelectizeInput(
+      session,
+      inputId = "projectPI",
+      choices = reactiveData$researchers[order(researchers$researcherName), ],
+      selected = dropdownMenuSelections[["projectPI"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "projectSupport1",
+      choices = reactiveData$researchers[order(researchers$researcherName), ],
+      selected = dropdownMenuSelections[["projectSupport1"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "projectSupport2",
+      choices = reactiveData$researchers[order(researchers$researcherName), ],
+      selected = dropdownMenuSelections[["projectSupport2"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "projectSupport3",
+      choices = reactiveData$researchers[order(researchers$researcherName), ],
+      selected = dropdownMenuSelections[["projectSupport3"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "projectSupport4",
+      choices = reactiveData$researchers[order(researchers$researcherName), ],
+      selected = dropdownMenuSelections[["projectSupport4"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "viewProjectsByResearcher",
+      choices = rbind(
+        data.frame(researcherID = NA,
+                   researcherUteid = NA,
+                   researcherName = NA,
+                   researcherEmail = NA,
+                   primaryDept = NA,
+                   secondaryDept = NA,
+                   value = "All",
+                   label = "All",
+                   stringsAsFactors = FALSE),
+        researchers[order(researchers$researcherName), ]
+      ),
+      selected = dropdownMenuSelections[["viewProjectsByResearcher"]],
+      server = TRUE
+    )
+  }
+)
+    
+    
+
+# update project dependent inputs -----------------------------------------
+observeEvent(
+  reactiveData$projects, {
+    # update selection inputs in the add time form
+    updateSelectizeInput(
+      session,
+      inputId = "timeProjectID",
+      choices = reactiveData$projects[order(projects$projectName), ],
+      selected = dropdownMenuSelections[["timeProjectID"]],
+      server = TRUE
+    )
+    
+    updateSelectizeInput(
+      session,
+      inputId = "viewTimeByProject",
+      choices = rbind(
+        data.frame(projectID = NA,
+                   projectName = NA,    # This is done in order to provide the "All" option
+                   bdshLead = NA,
+                   bdshSecondary = NA,
+                   projectPI = NA,
+                   projectSupport1 = NA,
+                   projectSupport2 = NA,
+                   projectSupport3 = NA,
+                   projectSupport4 = NA,
+                   projectDescription = NA,
+                   projectStatus = NA,
+                   projectDueDate = NA,
+                   value = "All",
+                   label = "All",
+                   stringsAsFactors = FALSE),
+        projects[order(projects$projectName), ]),
+      selected = dropdownMenuSelections[["viewTimeByProject"]],
+      server = TRUE
+    )
+    
+    updateSelectInput(
+      session,
+      inputId = "viewProjectsByStatus",
+      choices = c("All", unique(reactiveData$projects$projectStatus))
+    )
+  }
+)
+    
+# 2.3 Update Dropdown Menu Reactive ---------------------------------------
+    # Reactives that trigger after new data is loaded from the database that update
+    # the necessary input dropdown menus to reflect any changes in the database
+    updateSelectDropdownMenus <- 
+      reactive({
+      # When new employee data is fetched from database
+        # Update selection inputs in the Add Project form
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "bdshLead",
+        #   choices = reactiveData$employees[order(employees$employeeName), ],
+        #   selected = dropdownMenuSelections[["bdshLead"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "bdshSecondary",
+        #   choices = reactiveData$employees[order(employees$employeeName), ],
+        #   selected = dropdownMenuSelections[["bdshSecondary"]],
+        #   server = TRUE
+        # )
+        # 
+        # # Update selection inputs in the Add Time form
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "workBy",
+        #   choices = reactiveData$employees[order(employees$employeeName), ],
+        #   selected = dropdownMenuSelections[["workBy"]],
+        #   server = TRUE
+        # )
+        # 
+        # # Update selection inputs in View Projects
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "viewProjectsByEmployee",
+        #   choices = rbind(
+        #     data.frame(bdshID = NA,
+        #                employeeUteid = NA,    # This is done in order to provide the "All" option
+        #                employeeName = NA,
+        #                employeeEmail = NA,
+        #                degree = NA,
+        #                role = NA,
+        #                value = "All",
+        #                label = "All",
+        #                stringsAsFactors = FALSE),
+        #     employees[order(employees$employeeName), ]),
+        #   selected = dropdownMenuSelections[["viewProjectsByEmployee"]],
+        #   server = TRUE
+        # )
+        # 
+        # # Update selection inputs in View Time
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "viewTimeByEmployee",
+        #   choices = rbind(
+        #     data.frame(bdshID = NA,
+        #                employeeUteid = NA,    # This is done in order to provide the "All" option
+        #                employeeName = NA,
+        #                employeeEmail = NA,
+        #                degree = NA,
+        #                role = NA,
+        #                value = "All",
+        #                label = "All",
+        #                stringsAsFactors = FALSE),
+        #     employees[order(employees$employeeName), ]),
+        #   selected = dropdownMenuSelections[["viewTimeByEmployee"]],
+        #   server = TRUE
+        # )
         
         
       # When new project data is fetched from database
-        # update selection inputs in the add time form
-        updateSelectizeInput(
-          session,
-          inputId = "timeProjectID",
-          choices = reactiveData$projects[order(projects$projectName), ],
-          selected = dropdownMenuSelections[["timeProjectID"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "viewTimeByProject",
-          choices = rbind(
-            data.frame(projectID = NA,
-                       projectName = NA,    # This is done in order to provide the "All" option
-                       bdshLead = NA,
-                       bdshSecondary = NA,
-                       projectPI = NA,
-                       projectSupport1 = NA,
-                       projectSupport2 = NA,
-                       projectSupport3 = NA,
-                       projectSupport4 = NA,
-                       projectDescription = NA,
-                       projectStatus = NA,
-                       projectDueDate = NA,
-                       value = "All",
-                       label = "All",
-                       stringsAsFactors = FALSE),
-            projects[order(projects$projectName), ]),
-          selected = dropdownMenuSelections[["viewTimeByProject"]],
-          server = TRUE
-        )
-        
-        updateSelectInput(
-          session,
-          inputId = "viewProjectsByStatus",
-          choices = c("All", unique(reactiveData$projects$projectStatus))
-        )
+        # # update selection inputs in the add time form
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "timeProjectID",
+        #   choices = reactiveData$projects[order(projects$projectName), ],
+        #   selected = dropdownMenuSelections[["timeProjectID"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "viewTimeByProject",
+        #   choices = rbind(
+        #     data.frame(projectID = NA,
+        #                projectName = NA,    # This is done in order to provide the "All" option
+        #                bdshLead = NA,
+        #                bdshSecondary = NA,
+        #                projectPI = NA,
+        #                projectSupport1 = NA,
+        #                projectSupport2 = NA,
+        #                projectSupport3 = NA,
+        #                projectSupport4 = NA,
+        #                projectDescription = NA,
+        #                projectStatus = NA,
+        #                projectDueDate = NA,
+        #                value = "All",
+        #                label = "All",
+        #                stringsAsFactors = FALSE),
+        #     projects[order(projects$projectName), ]),
+        #   selected = dropdownMenuSelections[["viewTimeByProject"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectInput(
+        #   session,
+        #   inputId = "viewProjectsByStatus",
+        #   choices = c("All", unique(reactiveData$projects$projectStatus))
+        # )
         
         
       # When new researcher data is fetched from database
-        # update selection inputs in the add project form
-        updateSelectizeInput(
-          session,
-          inputId = "projectPI",
-          choices = reactiveData$researchers[order(researchers$researcherName), ],
-          selected = dropdownMenuSelections[["projectPI"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "projectSupport1",
-          choices = reactiveData$researchers[order(researchers$researcherName), ],
-          selected = dropdownMenuSelections[["projectSupport1"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "projectSupport2",
-          choices = reactiveData$researchers[order(researchers$researcherName), ],
-          selected = dropdownMenuSelections[["projectSupport2"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "projectSupport3",
-          choices = reactiveData$researchers[order(researchers$researcherName), ],
-          selected = dropdownMenuSelections[["projectSupport3"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "projectSupport4",
-          choices = reactiveData$researchers[order(researchers$researcherName), ],
-          selected = dropdownMenuSelections[["projectSupport4"]],
-          server = TRUE
-        )
-        
-        updateSelectizeInput(
-          session,
-          inputId = "viewProjectsByResearcher",
-          choices = rbind(
-            data.frame(researcherID = NA,
-                       researcherUteid = NA,
-                       researcherName = NA,
-                       researcherEmail = NA,
-                       primaryDept = NA,
-                       secondaryDept = NA,
-                       value = "All",
-                       label = "All",
-                       stringsAsFactors = FALSE),
-            researchers[order(researchers$researcherName), ]
-          ),
-          selected = dropdownMenuSelections[["viewProjectsByResearcher"]],
-          server = TRUE
-        )
+        # # update selection inputs in the add project form
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "projectPI",
+        #   choices = reactiveData$researchers[order(researchers$researcherName), ],
+        #   selected = dropdownMenuSelections[["projectPI"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "projectSupport1",
+        #   choices = reactiveData$researchers[order(researchers$researcherName), ],
+        #   selected = dropdownMenuSelections[["projectSupport1"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "projectSupport2",
+        #   choices = reactiveData$researchers[order(researchers$researcherName), ],
+        #   selected = dropdownMenuSelections[["projectSupport2"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "projectSupport3",
+        #   choices = reactiveData$researchers[order(researchers$researcherName), ],
+        #   selected = dropdownMenuSelections[["projectSupport3"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "projectSupport4",
+        #   choices = reactiveData$researchers[order(researchers$researcherName), ],
+        #   selected = dropdownMenuSelections[["projectSupport4"]],
+        #   server = TRUE
+        # )
+        # 
+        # updateSelectizeInput(
+        #   session,
+        #   inputId = "viewProjectsByResearcher",
+        #   choices = rbind(
+        #     data.frame(researcherID = NA,
+        #                researcherUteid = NA,
+        #                researcherName = NA,
+        #                researcherEmail = NA,
+        #                primaryDept = NA,
+        #                secondaryDept = NA,
+        #                value = "All",
+        #                label = "All",
+        #                stringsAsFactors = FALSE),
+        #     researchers[order(researchers$researcherName), ]
+        #   ),
+        #   selected = dropdownMenuSelections[["viewProjectsByResearcher"]],
+        #   server = TRUE
+        # )
       })
     
 
 # 2.4 Observer for updateSelectDropdownMenus ------------------------------
     # observeEvent which applies updateSelectDropdownMenus whenever the loadDatabase
     # function is called by monitoring updateOnLoad$dropdown reactive value 
-    observeEvent(
-      updateOnLoad$dropdown == TRUE, {
-        updateSelectDropdownMenus()
-        updateOnLoad$dropdown <- FALSE
-      })
+    # observeEvent(
+    #   updateOnLoad$dropdown == TRUE, {
+    #     updateSelectDropdownMenus()
+    #     updateOnLoad$dropdown <- FALSE
+    #   })
     
     
     
