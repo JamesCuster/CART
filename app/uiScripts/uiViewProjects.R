014149b398b655c7c648345706603a56df96a8f7
James Custer <jamescuster0121@gmail.com>
2019-08-20 15:48:12 -0500

    add the view for merged project data, view details link

--- app/uiScripts/uiViewProjects.R
+++ app/uiScripts/uiViewProjects.R
@@ -15,51 +15,10 @@ tabPanel(
     "Edit"
   ),
   
-  actionButton(
-    "removeProject",
-    "Delete"
-  ),
-  
   
   # Filter Projects inputs
   uiOutput("projectFilters"),
-  # div(
-  #   selectInput(
-  #     inputId = "viewProjectsByStatus",
-  #     label = "Project Status",
-  #     choices = c("All", "Active", "Closed", "Dormant"),
-  #     selected = "All"
-  #   ),
-  #   div(
-  #     selectizeInput(
-  #       inputId = "viewProjectsByEmployee",
-  #       label = "BDSH Staff",
-  #       choices = NULL,
-  #       selected = "All",
-  #       options = list(
-  #         placeholder = "All",
-  #         onInitialize = I("function() {this.setValue('All');}")
-  #       )
-  #     ),
-  #     style = "margin-left: 20px;"
-  #   ),
-  #   div(
-  #     selectizeInput(
-  #       inputId = "viewProjectsByResearcher",
-  #       label = "Reearcher",
-  #       choices = NULL,
-  #       selected = "All",
-  #       options = list(
-  #         placeholder = "All",
-  #         onInitialize = I("function() {this.setValue('All');}")
-  #       )
-  #     ),
-  #     style = "margin-left: 20px;"
-  #   ),
-  # 
-  # 
-  #   style = "display: flex; align-items: flex-start;"
-  # ),
+
 
 
   # Projects Data
