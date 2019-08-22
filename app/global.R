f7675f1f7bfdd0d471f5aab73286de6d2343e3e3
James Custer <jamescuster0121@gmail.com>
2019-08-22 15:24:53 -0500

    clean the global.r script

--- app/global.R
+++ app/global.R
@@ -11,21 +11,6 @@ dirPath <- "C:/Users/jmc6538/Desktop/BDSHProjectTracking"
 # Connect to database
 BDSHProjects <- dbConnect(SQLite(), paste0(dirPath, "/BDSHProjects.sqlite"))
 
-# reactives for the form data
-reactiveFormData <- 
-  reactiveValues(
-    projectFormData = NULL,
-    timeFormData = NULL,
-    researcherFormData = NULL,
-    employeeFormData = NULL
-  )
-
-# Reactives for the view tabs
-viewTables <- reactiveValues(
-  projects = NULL,
-  time = NULL
-)
-
 
 # Define the reactiveData reactive
 reactiveData <- reactiveValues()
@@ -37,20 +22,12 @@ loadDatabase <- function(tables = c("projects", "employees", "time", "researcher
   if ("projects" %in% tables) {
     reactiveData$projects <- tbl(BDSHProjects, "projects") %>% 
       collect() %>% 
-     # mutate(
-     #   value = projectID,
-     #   label = projectName
-     # ) %>%
       as.data.frame(stringsAsFactors = FALSE) 
   }
   # employees
   if ("employees" %in% tables) {
     reactiveData$employees <- tbl(BDSHProjects, "employees") %>% 
       collect()  %>% 
-     # mutate(
-     #   value = bdshID,
-     #   label = paste0(employeeName, " (", employeeUteid, ")")
-     # ) %>%
       as.data.frame(stringsAsFactors = FALSE)
   }
   # time
@@ -63,10 +40,6 @@ loadDatabase <- function(tables = c("projects", "employees", "time", "researcher
   if ("researchers" %in% tables) {
     reactiveData$researchers <- tbl(BDSHProjects, "researchers") %>% 
       collect() %>% 
-     # mutate(
-     #   value = researcherID,
-     #   label = paste0(researcherName, " (", researcherEmail, ")")
-     # ) %>%
       as.data.frame(stringsAsFactors = FALSE)
   }
   # modified
@@ -131,10 +104,6 @@ modalInputs <- function(ids, labels, type, values, df, choices) {
 
 
 
-
-
-# projects notes ----------------------------------------------------------
-
 # This function takes a data frame and creates a value/label data.frame for use
 # in selectInputs
 valueLabel <- function(df, value, label) {
@@ -148,67 +117,7 @@ valueLabel <- function(df, value, label) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Functions and helpers to create/manipulate delete/edit buttons ----------
-
-# function that adds the delete button to data.frames
-addDeleteEditLink <- function(df, idPrefix) {
-  # function to make delete link to be used with lapply
-  deleteLink <- function(rowID) {
-    # create inputId name
-    delID <- paste0(idPrefix, "Delete")
-    
-    as.character(
-      actionLink(
-        inputId = paste(idPrefix, rowID, sep = "_"),
-        label = "Delete",
-        onclick = 
-          paste0(
-            'Shiny.setInputValue(\"',
-            delID,
-            '\", this.id, {priority: "event"})'
-          )
-      )
-    )
-  }
-  
-  # function to make edit link to be used with lapply
-  editLink <- function(rowID) {
-    # create inputId name
-    editID <- paste0(idPrefix, "Edit")
-    
-    as.character(
-      actionLink(
-        inputId = paste(idPrefix, rowID, sep = "_"),
-        label = "Edit",
-        onclick = 
-          paste0(
-            'Shiny.setInputValue(\"',
-            editID,
-            '\", this.id, {priority: "event"})'
-          )
-      )
-    )
-  }
-  
-  df$Delete <- sapply(row.names(df), deleteLink)
-  df$Edit <- sapply(row.names(df), editLink)
-  return(df)
-}
-
-
-# Function that grabs the rowID for the row to be edited or deleted
+# Function that grabs the rowID for the row to view the details on
 parseDeleteEvent <- function(idstr) {
   res <- as.integer(sub(".*_([0-9]+)", "\\1", idstr))
   if (!is.na(res)) res
