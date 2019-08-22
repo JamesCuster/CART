0815c12c77377a9c2907118437ae642e4d764abc
James Custer <jamescuster0121@gmail.com>
2019-08-22 15:29:31 -0500

    clean server.r

--- app/server.R
+++ app/server.R
@@ -31,7 +31,6 @@ shinyServer(
         }
       )
     
-    
     # observe which applies the monitorDatabase reactive
     observe({
       monitorDatabase()
@@ -42,7 +41,7 @@ shinyServer(
 # Database Callback Functions ---------------------------------------------
     # These functions are used to create SQL queries which manipulate the database
     
-    # function that create insert SQL query
+    # creates insert SQL query
     insertCallback <- function(ids, tab) {
       insert <- paste0(
         "insert into ",
@@ -70,7 +69,7 @@ shinyServer(
     }
     
     
-    # function that create update SQL query
+    # Creates update SQL query
     updateCallback <- function(ids, df, row, idVar, tab) {
       ids <- ids[!ids %in% idVar]
       update <- paste0(
@@ -97,224 +96,27 @@ shinyServer(
       dbExecute(BDSHProjects, query)
     }
     
-
-
-
     
-# 2 Update Dropdown Selections ----------------------------------------------
-# This section allows for the dropdown menus to be updated as new data from the
-# database is loaded
- 
-# 2.1 update employee dependent inputs --------------------------------------
-#     observe({#c(input$addProject, input$editProject), {
-#       #browser()
-# #      reactiveData$employees, {
-#         # Update selection inputs in the Add Project form
-#         updateSelectizeInput(
-#           session,
-#           inputId = "bdshLead",
-#           choices = reactiveData$employees[order(reactiveData$employees$employeeName), ],
-#           selected = input[["bdshLead"]],
-#           server = TRUE
-#         )
-# 
-#         updateSelectizeInput(
-#           session,
-#           inputId = "bdshSecondary",
-#           choices = reactiveData$employees[order(reactiveData$employees$employeeName), ],
-#           selected = input[["bdshSecondary"]],
-#           server = TRUE
-#         )
-# 
-#         # Update selection inputs in the Add Time form
-#         updateSelectizeInput(
-#           session,
-#           inputId = "workBy",
-#           choices = reactiveData$employees[order(reactiveData$employees$employeeName), ],
-#           selected = input[["workBy"]],
-#           server = TRUE
-#         )
-# 
-#         # Update selection inputs in View Projects
-#         updateSelectizeInput(
-#           session,
-#           inputId = "viewProjectsByEmployee",
-#           choices = rbind(
-#             data.frame(bdshID = NA,
-#                        employeeUteid = NA,    # This is done in order to provide the "All" option
-#                        employeeName = NA,
-#                        employeeEmail = NA,
-#                        degree = NA,
-#                        role = NA,
-#                        value = "All",
-#                        label = "All",
-#                        stringsAsFactors = FALSE),
-#             reactiveData$employees[order(reactiveData$employees$employeeName), ]),
-#           selected = input[["viewProjectsByEmployee"]],
-#           server = TRUE
-#         )
-# 
-#         # Update selection inputs in View Time
-#         updateSelectizeInput(
-#           session,
-#           inputId = "viewTimeByEmployee",
-#           choices = rbind(
-#             data.frame(bdshID = NA,
-#                        employeeUteid = NA,    # This is done in order to provide the "All" option
-#                        employeeName = NA,
-#                        employeeEmail = NA,
-#                        degree = NA,
-#                        role = NA,
-#                        value = "All",
-#                        label = "All",
-#                        stringsAsFactors = FALSE),
-#             reactiveData$employees[order(reactiveData$employees$employeeName), ]),
-#           selected = input[["viewTimeByEmployee"]],
-#           server = TRUE
-#         )
-#       }
-#     )
-# 
-# 
-# # 2.1 update researcher dependent inputs ------------------------------------
-# observeEvent(
-#   reactiveData$researchers, {
-#     # update selection inputs in the add project form
-#     updateSelectizeInput(
-#       session,
-#       inputId = "projectPI",
-#       choices = reactiveData$researchers[order(reactiveData$researchers$researcherName), ],
-#       selected = input[["projectPI"]],
-#       server = TRUE
-#     )
-# 
-#     updateSelectizeInput(
-#       session,
-#       inputId = "projectSupport1",
-#       choices = reactiveData$researchers[order(reactiveData$researchers$researcherName), ],
-#       selected = input[["projectSupport1"]],
-#       server = TRUE
-#     )
-# 
-#     updateSelectizeInput(
-#       session,
-#       inputId = "projectSupport2",
-#       choices = reactiveData$researchers[order(reactiveData$researchers$researcherName), ],
-#       selected = input[["projectSupport2"]],
-#       server = TRUE
-#     )
-# 
-#     updateSelectizeInput(
-#       session,
-#       inputId = "projectSupport3",
-#       choices = reactiveData$researchers[order(reactiveData$researchers$researcherName), ],
-#       selected = input[["projectSupport3"]],
-#       server = TRUE
-#     )
-# 
-#     updateSelectizeInput(
-#       session,
-#       inputId = "projectSupport4",
-#       choices = reactiveData$researchers[order(reactiveData$researchers$researcherName), ],
-#       selected = input[["projectSupport4"]],
-#       server = TRUE
-#     )
-# 
-#     # Update selection inputs in view projects
-#     updateSelectizeInput(
-#       session,
-#       inputId = "viewProjectsByResearcher",
-#       choices = rbind(
-#         data.frame(researcherID = NA,
-#                    researcherUteid = NA,
-#                    researcherName = NA,
-#                    researcherEmail = NA,
-#                    primaryDept = NA,
-#                    secondaryDept = NA,
-#                    value = "All",
-#                    label = "All",
-#                    stringsAsFactors = FALSE),
-#         reactiveData$researchers[order(reactiveData$researchers$researcherName), ]
-#       ),
-#       selected = input[["viewProjectsByResearcher"]],
-#       server = TRUE
-#     )
-#   }
-# )
-# 
-# 
-# # update project dependent inputs -----------------------------------------
-# observeEvent(
-#   reactiveData$projects, {
-#     # update selection inputs in the add time form
-#     updateSelectizeInput(
-#       session,
-#       inputId = "timeProjectID",
-#       choices = reactiveData$projects[order(reactiveData$projects$projectName), ],
-#       selected = input[["timeProjectID"]],
-#       server = TRUE
-#     )
-# 
-#     # update selection inputs in view time
-#     updateSelectizeInput(
-#       session,
-#       inputId = "viewTimeByProject",
-#       choices = rbind(
-#         data.frame(projectID = NA,
-#                    projectName = NA,    # This is done in order to provide the "All" option
-#                    bdshLead = NA,
-#                    bdshSecondary = NA,
-#                    projectPI = NA,
-#                    projectSupport1 = NA,
-#                    projectSupport2 = NA,
-#                    projectSupport3 = NA,
-#                    projectSupport4 = NA,
-#                    projectDescription = NA,
-#                    projectStatus = NA,
-#                    projectDueDate = NA,
-#                    value = "All",
-#                    label = "All",
-#                    stringsAsFactors = FALSE),
-#         reactiveData$projects[order(reactiveData$projects$projectName), ]),
-#       selected = input[["viewTimeByProject"]],
-#       server = TRUE
-#     )
-# 
-#     # update selection inputs in view projects
-#     updateSelectInput(
-#       session,
-#       inputId = "viewProjectsByStatus",
-#       choices = c("All", unique(reactiveData$projects$projectStatus))
-#     )
-#   }
-# )
-#     
-#     
-#     
+    
 # 3 Server Scripts ----------------------------------------------------------
-    # serverAddTime
-    source(
-      paste0(dirPath, "/app/serverScripts/serverTime.r"),
-      local = TRUE
-    )
     
-    # serverAddPeople
-    source(
-      paste0(dirPath, "/app/serverScripts/serverPeople.r"),
-      local = TRUE
-    )
-    
-    # ViewProjects
+    # Projects
     source(
       paste0(dirPath, "/app/serverScripts/serverProjects.r"),
       local = TRUE
     )
     
-    # # serverViewTime
-    # source(
-    #   paste0(dirPath, "/app/serverScripts/serverViewTime.r"),
-    #   local = TRUE
-    # )
+    # Time
+    source(
+      paste0(dirPath, "/app/serverScripts/serverTime.r"),
+      local = TRUE
+    )
+    
+    # People
+    source(
+      paste0(dirPath, "/app/serverScripts/serverPeople.r"),
+      local = TRUE
+    )
     
     
     
