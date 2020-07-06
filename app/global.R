15dacb139e3cb46d610711e230730d67d2c32170
James Custer <jamescuster0121@gmail.com>
2020-07-06 15:50:00 -0500

    add alternative method for dirPath if app is not launched from .bat file.

--- app/global.R
+++ app/global.R
@@ -6,8 +6,10 @@ library(shinyjs)
 
 
 # Database related functions ----------------------------------------------
-  # dirPath is defined in run.R and supplied via an argument in the CART.bat call
-  # to rscript
+  # dirPath is defined in run.R and supplied via an argument in the CART.bat
+  # call to rscript. However, if dirPath is not defined up to this point, then
+  # need it defined locally, that is what this code below does
+dirPath <- ifelse(exists("dirPath"), dirPath, "C:/Users/jmc6538/Documents/BDSHProjects/CART")
 
 # Connect to database
 BDSHProjects <- dbConnect(dbDriver("SQLite"), paste0(dirPath, "/BDSHProjects.sqlite"))
