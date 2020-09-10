5d9a6aa6d4a989d5821e29cd8432e16b4925576a
James Custer <jamescuster0121@gmail.com>
2020-09-10 15:12:01 -0500

    add funded to time table and update database

--- fundedDBClean.R
+++ fundedDBClean.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(RSQLite)
 
+
+# Impute funded in Projects -----------------------------------------------
+
 # read in funded CSV
 funded <- read.csv("C:/Users/jmc6538/Documents/BDSHProjects/CART/FundedProjects.csv", 
                    stringsAsFactors = FALSE)
@@ -12,7 +15,7 @@ dirPath <- ifelse(exists("dirPath"), dirPath, "C:/Users/jmc6538/Documents/BDSHPr
 # Connect to database
 BDSHProjects <- dbConnect(dbDriver("SQLite"), paste0(dirPath, "/BDSHProjects.sqlite"))
 
-# read in researcher table
+# read in projects table
 projects <- tbl(BDSHProjects, "projects") %>% 
   collect() %>% 
   as.data.frame(stringsAsFactors = FALSE)
@@ -27,4 +30,29 @@ table(projects$projectFunded, p, useNA = "ifany")
 
 projects$projectFunded <- p
 
-dbWriteTable(BDSHProjects, "projects", value = projects, append = FALSE, overwrite = TRUE)
+dbWriteTable(BDSHProjects, "projects", value = projects, append = FALSE, 
+             overwrite = TRUE)
+
+
+
+
+# Add funded to time ------------------------------------------------------
+
+# read in time table
+time <- tbl(BDSHProjects, "time") %>% 
+  collect() %>% 
+  as.data.frame(stringsAsFactors = FALSE)
+
+head(time)
+
+time$timeFunded <- ifelse(time$timeProjectID %in% funded$ProjectID, "Funded", "Unfunded")
+table(p, useNA = "always")
+
+# Reorder variables
+time <- time[, c("timeID", "timeProjectID", "workBy", "dateOfWork", "dateOfEntry",
+         "timeFunded", "workTime", "workTimeCategory", "workCategory", 
+         "workDescription")]
+
+
+dbWriteTable(BDSHProjects, "time", value = time, append = FALSE, 
+             overwrite = TRUE)
